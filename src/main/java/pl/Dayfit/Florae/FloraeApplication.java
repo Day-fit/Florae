2be09bece83d4d8d4426ceb35dc9000e2d20e2d8
Dawid Lech Socha <151644314+Day-fit@users.noreply.github.com>
2025-04-24 10:19:39 +0000

    innitialize from SpringInitializr

---
+++ src/main/java/pl/Dayfit/Florae/FloraeApplication.java
@@ -0,0 +1,13 @@
+package pl.Dayfit.Florae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloraeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloraeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/Dayfit/Florae/FloraeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.Dayfit.Florae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloraeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
