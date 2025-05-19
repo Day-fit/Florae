c280006dd8718a0f6e92b06a99eb3b99ea1604e9
jasq12345 <na.infromatyke153@gmail.com>
2025-05-19 16:18:59 +0200

    Refactor build process and GitHub Pages deployment setup
    Updated Vite configuration for custom output directory and build inputs. Simplified Tailwind CSS import in index.css. Revised GitHub Actions workflow for streamlined deployment of the React app to GitHub Pages.

--- florae-frontend/vite.config.js
+++ florae-frontend/vite.config.js
@@ -3,6 +3,19 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "Florae",
+
+  build: {
+    outDir: "dist",
+
+    rollupOptions: {
+      input: {
+        input: "./index.html",
+        main: "src/main.jsx",
+      },
+    }
+  },
+
   plugins: [
     react(),
     tailwindcss(),
