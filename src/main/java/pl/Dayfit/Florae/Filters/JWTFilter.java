0d3c335839d4a654af154f59a918b47d1ea29dad
Dawid Lech Socha <xDayfit@gmail.com>
2025-05-21 22:29:20 +0200

    minor changes

--- src/main/java/pl/Dayfit/Florae/Filters/JWTFilter.java
+++ src/main/java/pl/Dayfit/Florae/Filters/JWTFilter.java
@@ -63,7 +63,6 @@ public class JWTFilter extends OncePerRequestFilter {
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-                return;
             }
         }
 
