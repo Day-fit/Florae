c8fe70f25b5c09558e33f4ba889e20b15ca171dd
Dawid Lech Socha <xDayfit@gmail.com>
2025-06-13 15:56:16 +0200

    Include `AuthenticationException` from Spring Security in `GlobalExceptionHandler`.

--- src/main/java/pl/Dayfit/Florae/ExceptionHandlers/GlobalExceptionHandler.java
+++ src/main/java/pl/Dayfit/Florae/ExceptionHandlers/GlobalExceptionHandler.java
@@ -2,13 +2,13 @@ package pl.Dayfit.Florae.ExceptionHandlers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import pl.Dayfit.Florae.Exceptions.ApiKeyAssociationException;
 
-import javax.naming.AuthenticationException;
 import java.util.Map;
 
 @RestControllerAdvice
