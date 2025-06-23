ece4cf23298f113aaf48c4234d517cb47f3289c0
ksaczy <ksaczy@proton.me>
2025-06-23 20:32:52 +0200

    Update footer-data.js

--- florae-frontend/src/util/footer-data.js
+++ florae-frontend/src/util/footer-data.js
@@ -1,38 +1,12 @@
 export const footerData = [
   {
-    name: 'Home',
-    subpoints: [
-      'Get started',
-      'Why plant lovers trust us',
-      'Wireframing',
-      'Diagramming',
-      'Brainstorming',
-      'Online whiteboard',
-      'Contact us',
-    ],
+    name: 'Florae – Nurture your plants, effortlessly',
   },
   {
-    name: 'Plants',
-    subpoints: [
-      'Design',
-      'Prototyping',
-      'Development features',
-      'Design systems',
-      'Collaboration features',
-      'Design process',
-      'FigJam',
-    ],
+    name: 'Contact Us: Florae@gmail.com',
+    
   },
   {
-    name: 'Devices',
-    subpoints: [
-      'Blog',
-      'Best practices',
-      'Colors',
-      'Color wheel',
-      'Support',
-      'Developers',
-      'Resource library',
-    ],
+    name: 'Grow, track, and thrive with Florae !',   
   },
 ];
