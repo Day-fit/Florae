1cc5a09d931ec6d4414a442c13cca279007a1133
Dawid Lech Socha <151644314+Day-fit@users.noreply.github.com>
2025-06-23 20:39:23 +0200

    Update footer-data.js

--- florae-frontend/src/util/footer-data.js
+++ florae-frontend/src/util/footer-data.js
@@ -3,8 +3,7 @@ export const footerData = [
     name: 'Florae – Nurture your plants, effortlessly',
   },
   {
-    name: 'Contact Us: Florae@gmail.com',
-    
+    name: 'Have an issue? No problem — contact us via GitHub Issues.',
   },
   {
     name: 'Grow, track, and thrive with Florae !',   
