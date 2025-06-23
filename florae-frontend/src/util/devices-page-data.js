cb448c4d5e79baa1c24afc9b70de4c3b90a5c675
ksaczy <ksaczy@proton.me>
2025-06-23 20:14:15 +0200

    Update devices-page-data.js
    ustawiony title i tekst kiedy nie jest sie zalogowanym oraz tekst na butttonie

--- florae-frontend/src/util/devices-page-data.js
+++ florae-frontend/src/util/devices-page-data.js
@@ -1,10 +1,11 @@
 export const devicesVisitorContent = {
-  title: 'Title',
+  title: 'Devices',
   paragraph:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit maximus diam, id fringilla ipsum faucibus eu. Integer et commodo ante. Maecenas sem ligula, venenatis vitae ex nec, fringilla sodales arcu. Phasellus a ullamcorper ante. Nunc in imperdiet mauris, eu fringilla dui. Sed egestas, ex sit amet convallis viverra, tortor lectus placerat sem, eget ultricies libero ante vitae turpis. Nullam mollis tincidunt aliquet. Mauris accumsan fermentum blandit. Ut eleifend mi purus, vel sodales ipsum viverra in. Nullam egestas nibh quis lectus imperdiet lacinia. Donec pulvinar luctus felis ac egestas. In nibh neque, imperdiet vitae convallis id, suscipit sit amet erat. Etiam justo augue, vulputate nec est vel, porta suscipit augue. Sed vitae malesuada nisl. Ut eu semper nunc. Mauris consequat, urna eu commodo auctor, mi nunc faucibus elit, eget ultricies risus ante a lectus.',
+    'Connect and manage devices that help monitor and maintain your plants and Keep your plants thriving with automated tools and real-time insights.',
   btnText: 'Get Started',
 };
 export const devicesGuestContent = {
   title: 'Title',
   paragraph: 'restart if not renders (click f5) and u cant add api to a plant more than 1 time ',
+  btnText: 'Configure',
 };
